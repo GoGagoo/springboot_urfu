a765ee8af1dd38e5a7d4dc4bf06386908de0b1ca
Gagik Antonyan <gagika57@gmail.com>
2024-09-23 17:35:49 +0500

    WIP

---
+++ MySecondTestApp/src/main/java/ru/arkhipov/MySecondTestApp/MySecondTestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.arkhipov.MySecondTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecondTestAppApplication.class, args);
+	}
+
+}
